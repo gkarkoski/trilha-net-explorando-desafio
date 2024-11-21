d222458d82f96238cc1f6d5becddf86d26fd07df
gkarkoski <guilherme.karkoski@gmail.com>
2024-11-20 21:16:08 -0300

    Projeto finalizado, capacidade suite, exceção de capacidade, contagem hospedes, calc valor diária.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,8 +15,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
+            
             if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
@@ -25,8 +24,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 throw new Exception("Capacidade da suíte menor que o número de hóspedes!");
 
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                
             }
         }
 
@@ -37,26 +35,22 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
+            
             return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-            valor = DiasReservados * Suite.ValorDiaria;
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
+           
+            decimal valorDiaria = 0;
+            valorDiaria = DiasReservados * Suite.ValorDiaria;
+           
             if (DiasReservados >= 10)
             {
-                valor -= valor * 0.1M;
+                valorDiaria -= valorDiaria * 0.1M;
             }
 
-            return valor;
+            return valorDiaria;
         }
     }
 }
